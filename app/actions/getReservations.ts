d8d93914036f84a53c893ed8487977bd02d798a3
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-09-01 10:39:26 +0300

    Fetch reservations for different params

---
+++ app/actions/getReservations.ts
@@ -0,0 +1,42 @@
+import prisma from "@/app/libs/prismadb";
+
+type IParams = {
+  listingId?: string;
+  userId?: string;
+  authorId?: string;
+}
+
+export default async function getReservations(params: IParams) {
+
+  const { listingId, userId, authorId } = params;
+
+  if (!listingId && !userId && !authorId) throw new Error('Missing params')
+
+  let query: any = {};
+
+  if (listingId)
+    query.listingId = listingId;
+
+  if (userId)
+    query.userId = userId
+
+  if (authorId)
+    query.authorId = authorId
+
+  try {
+    const listings = await prisma.reservation.findMany({
+      where: query,
+      include: {
+        listing: true,
+      },
+      orderBy: {
+        createdAt: 'desc'
+      }
+    });
+
+    return listings;
+
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
