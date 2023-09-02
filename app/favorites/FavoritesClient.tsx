0e21a8805983bf4862c8c7bd8ce7e2d9ff314c1d
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-09-02 23:49:08 +0300

    Add Header to the pages

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -4,6 +4,7 @@ import { Listing, User } from "@prisma/client";
 
 import Container from "../components/Container";
 import ListingCard from "../components/listings/ListingCard";
+import Heading from "../components/navbar/Heading";
 
 type Props = {
   favorites: Listing[];
@@ -13,8 +14,12 @@ type Props = {
 export default function FavoritesClient({ favorites, currentUser }: Props) {
   return (
     <Container>
+      <Heading
+        title="My Favorites"
+        subtitle="Places that I have marked as favorites"
+      />
       <div
-        className="grid grid-cols-1 gap-8
+        className="mt-8 grid grid-cols-1 gap-8
           sm:grid-cols-2 
           md:grid-cols-3
           lg:grid-cols-4
--- app/properties/PropertiesClient.tsx
+++ app/properties/PropertiesClient.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Listing, User } from "@prisma/client";
 import Container from "../components/Container";
 import ListingCard from "../components/listings/ListingCard";
+import Heading from "../components/navbar/Heading";
 
 type Props = {
   properties: Listing[];
@@ -11,8 +12,9 @@ type Props = {
 export default function PropertiesClient({ properties, currentUser }: Props) {
   return (
     <Container>
+      <Heading title="My Properties" subtitle="Properties I belong to" />
       <div
-        className="grid grid-cols-1 gap-8
+        className="mt-8 grid grid-cols-1 gap-8
       sm:grid-cols-2 
       md:grid-cols-3
       lg:grid-cols-4
