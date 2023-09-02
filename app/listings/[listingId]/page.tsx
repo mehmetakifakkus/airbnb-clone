bc5fe662f801746178a61a83f3bb7650c3c79e8a
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-09-02 23:49:52 +0300

    Generate static params for listings

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -3,10 +3,20 @@ import EmptyState from "@/app/components/EmptyState";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import ListingClient from "./ListingClient";
 import getReservations from "@/app/actions/getReservations";
+import getListings from "@/app/actions/getListings";
+import { Listing } from "@prisma/client";
 
 type Props = {
   params: { listingId: string };
 };
+
+export async function generateStaticParams() {
+  const listings = await getListings();
+
+  return listings.map((listing: Listing) => ({
+    listingId: listing.id,
+  }));
+}
 
 export default async function ListingPage({ params }: Props) {
   const currentUser = await getCurrentUser();
